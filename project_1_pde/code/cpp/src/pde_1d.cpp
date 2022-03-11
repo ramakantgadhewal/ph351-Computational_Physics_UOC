596f5309e2276990f72d7c019f1131421e0da75e
Jason Andronis <iasona98@gmail.com>
2022-03-12 01:01:31 +0200

    simple recompilation

--- project_1_pde/code/cpp/src/pde_1d.cpp
+++ project_1_pde/code/cpp/src/pde_1d.cpp
@@ -1,64 +1,10 @@
+#include "../../../../universal_code/cpp/src/FuncTools.h"
 #include <iostream>
-#include <cmath>
-#include <vector>
-#include <array>
 #include <fstream>
 #include <filesystem>
 using namespace std;
 
-const double PI = M_PI;
-
-template<typename T>
-vector<T> linspace(double min, double max, int n)
-{
-    vector<T> result;
-    // vector iterator
-    int iterator = 0;
-
-    for (int i = 0; i <= n-2; i++)	
-    {
-        T temp = min + i*(max-min)/(floor((T)n) - 1);
-        result.insert(result.begin() + iterator, temp);
-        iterator += 1;
-    }
-
-    result.insert(result.begin() + iterator, max);
-    return result;
-}
-
-template<class T>
-vector<T> zeros_1d(int n)
-{
-    vector<T> result;
-    // vector iterator
-    int iterator = 0;
-
-    for (int i = 0; i <= n-1; i++)	
-    {
-        T temp = 0;
-        result.insert(result.begin() + iterator, temp);
-        iterator += 1;
-    }
-
-    return result;
-}
-
-template<class T>
-vector<T> ones_1d(int n)
-{
-    vector<T> result;
-    // vector iterator
-    int iterator = 0;
-
-    for (int i = 0; i <= n-1; i++)	
-    {
-        T temp = 1;
-        result.insert(result.begin() + iterator, temp);
-        iterator += 1;
-    }
-
-    return result;
-}
+double PI = M_PI;
 
 int main()
 {
