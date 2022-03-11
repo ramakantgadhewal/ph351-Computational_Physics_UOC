ce943a8d2fdf5d9a9f804c5cce1f5d597b0cb15f
Jason Andronis <iasona98@gmail.com>
2022-03-12 01:21:55 +0200

    adjust to new directory

--- project_1_pde/code/python/plot.py
+++ project_1_pde/code/python/plot.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import sys
-sys.path.insert(0,'../../../')
+sys.path.insert(0,'../')
 from universal_code.python.saveFig import saveFig
 
 def main():
     # 1D Case
-    data_path = "../cpp/data/"
+    data_path = "data_1d/"
     energy = np.loadtxt(data_path+"energy_data_1d.dat")
     field = np.loadtxt(data_path+"field_1d.dat")
     potential = np.loadtxt(data_path+"potential_1d.dat")
@@ -18,28 +18,28 @@ def main():
     ax1.set_xlabel("Steps")
     ax1.set_ylabel("Energy")
     # plt.title("Energy of the system")
-    saveFig(fig_id="energy_1d_plot", destination="data/")
+    saveFig(fig_id="energy_1d_plot", destination="data_1d/")
     
     _, ax2 = plt.subplots()
     ax2.plot(field)
     ax2.set_xlabel("N")
     ax2.set_ylabel("Electric Field")
     # plt.title("Energy of the system")
-    saveFig(fig_id="field_1d_plot", destination="data/")
+    saveFig(fig_id="field_1d_plot", destination="data_1d/")
     
     _, ax3 = plt.subplots()
     ax3.plot(potential)
     ax3.set_xlabel("N")
     ax3.set_ylabel("Potential")
     # plt.title("Energy of the system")
-    saveFig(fig_id="potential_1d_plot", destination="data/")
+    saveFig(fig_id="potential_1d_plot", destination="data_1d/")
     
     _, ax4 = plt.subplots()
     ax4.plot(source)
     ax4.set_xlabel("N")
     ax4.set_ylabel("Source")
     # plt.title("Energy of the system")
-    saveFig(fig_id="source_1d_plot", destination="data/")
+    saveFig(fig_id="source_1d_plot", destination="data_1d/")
     
 if __name__=="__main__":
     main()
