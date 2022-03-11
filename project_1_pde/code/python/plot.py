5c7df55f65f2fe639dbdbc940e9796934ead85f4
JAndronis <iasona98@gmail.com>
2022-03-11 14:13:06 +0200

    basic data loading and plots

--- project_1_pde/code/python/plot.py
+++ project_1_pde/code/python/plot.py
@@ -1,9 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 def main():
-    # main program goes here
-    pass
+    # 1D Case
+    data_path = "../cpp/data/"
+    energy = np.loadtxt(data_path+"energy_data_1d.dat")
+    field = np.loadtxt(data_path+"field_1d.dat")
+    potential = np.loadtxt(data_path+"potential_1d.dat")
+    source = np.loadtxt(data_path+"source_1d.dat")
+    
+    plt.plot(field)
+    plt.show()
 
 if __name__=="__main__":
     main()
