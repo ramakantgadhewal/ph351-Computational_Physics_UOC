0bb8c8b048887fa3dd22f8436942c17e7c4b31c2
JAndronis <iasona98@gmail.com>
2022-04-06 17:58:51 +0300

    removed eigen general directory and added some stuff plus binary of project 2

--- project_2_ed/code/cpp/src/ed.cpp
+++ project_2_ed/code/cpp/src/ed.cpp
@@ -5,19 +5,18 @@
 #include <filesystem>
 #include <fstream>
 
-// int oldmain();
-
 int main()
 {
     // Declare and initialize problem variables
-    const int sites = 100, time = 100;
-    const double hopping = 0.4;
-    const Eigen::VectorXf x = Eigen::VectorXf::LinSpaced(sites,-1,1);
+    const int sites = 50, time = 100;
+    const double hopping = 1;
+    const Eigen::ArrayXf x = Eigen::ArrayXf::LinSpaced(sites,-1,1);
     Eigen::MatrixXf H(sites, sites);
     Eigen::MatrixXf T(sites, sites);
     Eigen::MatrixXf V(sites, sites);
-    Eigen::VectorXf c(sites);
+    Eigen::ArrayXf c(sites);
 
+    // Set all elements to 0
     H.setZero();
     T.setZero();
     V.setZero();
@@ -26,186 +25,109 @@ int main()
     {
         for(int j=0; j<H.cols(); j++)
         {
-            if(i==j) V(i,j) = 0.5*std::pow(x[i],2);
+            if(i==j) V(i,j) = 0.5*std::pow(x(i),2);
             if(i==j-1) T(i,j) = -hopping;
             if(j==i-1) T(i,j) = -hopping;
             if(i==j+1) T(i,j) = -hopping;
             if(j==i+1) T(i,j) = -hopping;
             H(i,j) = T(i,j)+V(i,j);
-            c(i) = std::exp(-std::pow(x(i),2));
+            c(i) = std::exp(-std::pow(x(i), 2));
         }
     }
+    
+    // Normalize initial coefficients
+    c.matrix().normalize();
 
-    c.normalize(); 
+    // Derive eigenvectors and eigenvalues from Hamiltonian
     Eigen::EigenSolver<Eigen::MatrixXf> es(H);
     Eigen::MatrixXf eigvecs = es.eigenvectors().real();
     Eigen::VectorXf eigvals = es.eigenvalues().real();
 
-    Eigen::MatrixXf yr = Eigen::MatrixXf::Zero(sites,time);
-    Eigen::MatrixXf yi = Eigen::MatrixXf::Zero(sites,time);
+    Eigen::ArrayXXf yr(sites, time);
+    Eigen::ArrayXXf yi(sites, time);
+    float sum_mr{0}, sum_mi{0};
 
-    for(int t=0; t<time; t++)
+    for (int k = 0; k < sites; k++)
     {
-        for(int k=0; k<sites; k++)
+        for (int t = 0; t < time; t++)
         {
-            for(int i=0; i<sites; i++)
+            sum_mr = 0;
+            sum_mi = 0;
+            for (int i = 0; i < eigvecs.cols(); i++)
             {
-                for(int m=0; m<sites; m++)
+                for (int m = 0; m < eigvecs.rows(); m++)
                 {
-                    yr(k,t) += c(i)*eigvecs(i,m)*eigvecs(k,m)*std::cos(eigvals(m)*t);
-                    yi(k,t) -= c(i)*eigvecs(i,m)*eigvecs(k,m)*std::sin(eigvals(m)*t);
+                    float temp = eigvals(m) * t;
+                    sum_mr += c(i) * eigvecs(i, m) * eigvecs(k, m) * std::cos(temp);
+                    sum_mi += c(i) * eigvecs(i, m) * eigvecs(k, m) * std::sin(temp);
                 }
             }
+            yr(k, t) = sum_mr;
+            yi(k, t) = -sum_mi;
         }
     }
 
+    Eigen::ArrayXXf P = yr.abs().pow(2) + yi.abs().pow(2);
+
+    // Arrays with _ in front are helper arrays
+    Eigen::ArrayXXf _mean_x(sites, time);
+    // Column wise operation, multiply every column with x
+    _mean_x = P.colwise() * x;
+    // every element of mean_x is the mean value of x at a specific timestep
+    Eigen::ArrayXf mean_x = _mean_x.colwise().sum();
+
+    // same goes here
+    Eigen::ArrayXXf _mean_x_squared(sites, time);
+    _mean_x_squared = P.colwise() * x.array().pow(2);
+    Eigen::ArrayXf mean_x_squared = _mean_x_squared.colwise().sum();
+
+    Eigen::ArrayXf variance = mean_x_squared - (mean_x * mean_x);
+
+    /*-----------------------------------------------------------------------------------*/
+
+    // Save files for plotting
     namespace fs = std::filesystem;
-    fs::path p=fs::current_path();
+    fs::path p = fs::current_path();
     fs::create_directory(p+="/data");
 
-    std::ofstream YR{"data/YR.dat"};
-    for(int k=0; k<sites; k++)
+    std::ofstream prob{ "data/P.dat" };
+    for (int k = 0; k < sites; k++)
     {
-        for(int t=0; t<time; t++)
+        for (int t = 0; t < time; t++)
         {
-            YR << yr(k,t) << " ";
+            prob << P(k, t) << " ";
         }
-        YR << "\n";
+        prob << "\n";
     }
-    YR.close();
+    prob.close();
 
-    std::ofstream YI{"data/YI.dat"};
-    for(int k=0; k<sites; k++)
+    std::ofstream potential{ "data/V.dat" };
+    for (int k = 0; k < sites; k++)
     {
-        for(int t=0; t<time; t++)
-        {
-            YI << yi(k,t) << " ";
-        }
-        YI << "\n";
+        potential << V.diagonal()(k) << "\n";
     }
-    YI.close();
+    potential.close();
+
+    std::ofstream initial{ "data/init.dat" };
+    for (int k = 0; k < sites; k++)
+    {
+        initial << c(k) << "\n";
+    }
+    initial.close();
+
+    std::ofstream m_x{ "data/mean_x.dat" };
+    for (int t = 0; t < time; t++)
+    {
+        m_x << mean_x(t) << "\n";
+    }
+    m_x.close();
+
+    std::ofstream var{ "data/variance.dat" };
+    for (int t = 0; t < time; t++)
+    {
+        var << variance(t) << "\n";
+    }
+    var.close();
+
     return 0;
-};
-
-// int oldmain()
-// {
-//     // Declare and initialize problem variables
-//     const int sites = 10, time = 100;
-//     const double hopping = 1;
-//     // grid initialization
-//     std::vector<double> x = functools::linspace<double>(0,sites,sites);
-//     // Hamiltonian, Kinetic Energy, Potential Energy
-//     double H[sites*sites], K[sites*sites], V[sites*sites];
-//     int index;
-//     double norm = 0;
-
-//     // create array for orthogonal transformation matrix
-//     double z[sites*sites];
-//     // create array for diagonal elements
-//     double d[sites];
-//     // create array for subdiagonal elements
-//     double e[sites-1];
-//     e[0] = 1;
-//     // initial probability amplidute
-//     double c[sites];
-
-//     for(int i=0; i<sites; i++)
-//     {
-//         for(int j=0; j<sites; j++)
-//         {
-//             index = i+j*sites;
-//             K[index] = 0;
-//             V[index] = 0;
-//             if(i==j) V[index] = 0.5*std::pow(x[i],2);
-//             if(i==j+1) K[index] = -hopping;
-//             else if(j==i+1) K[index] = -hopping;
-//             else if(i==j-1) K[index] = -hopping;
-//             else if(j==i-1) K[index] = -hopping;
-//             H[index] = V[index]+K[index];
-//             std::cout << H[index] << " ";
-//         }
-//         std::cout << "\n";
-//         norm += std::exp(-2*std::pow(x[i],2));
-//     }
-
-//     for(int i=0; i<sites; i++)
-//     {
-//         c[i] = std::exp(-std::pow(x[i],2))/std::sqrt(norm);
-//     }
-//     /* 
-//     tred2 will convert:
-//         d -> diagonal elements of H
-//         e -> subdiagonal elements of H
-//         z -> orthogonal transformation matrix
-//     */
-//     // tred2(sites, H, d, e, z);
-
-//     // // construct the tridiagonal matrix
-//     // double a2[sites*sites];
-//     // for(int i=0; i<sites; i++)
-//     // {
-//     //     for(int j=0; j<sites; j++)
-//     //     {
-//     //         index = i+j*sites;
-//     //         if(i==j) a2[index] = d[i];
-//     //         else if(j==i-1) a2[index] = e[i];
-//     //         else if(j==i+1) a2[index] = e[j];
-//     //         else a2[index] = 0;
-//     //     }
-//     // }
-
-//     /* 
-//     tql2 will convert (assuming ierr==0):
-//         d -> eigenvalues of H
-//         e -> destroyed
-//         z -> eigenvectors of H
-//     */
-//     int ierr = tql2(sites, d, e, z);
-
-//     double yr[sites][time];
-//     double yi[sites][time];
-
-//     for(int t=0; t<time; t++)
-//     {
-//         for(int k=0; k<sites; k++)
-//         {
-//             for(int i=0; i<sites; i++)
-//             {
-//                 for(int m=0; m<sites; m++)
-//                 {
-//                     yr[k][t] += c[i]*z[i+m*sites]*z[k+m*sites]*std::cos(d[m]*t);
-//                     yi[k][t] -= c[i]*z[i+m*sites]*z[k+m*sites]*std::sin(d[m]*t);
-//                 }
-//             }
-//         }
-//     }
-
-//     std::cout << std::endl;
-
-//     namespace fs = std::filesystem;
-//     fs::path p=fs::current_path();
-//     fs::create_directory(p+="/data");
-
-//     std::ofstream YR{"data/YR.dat"};
-//     for(int k=0; k<sites; k++)
-//     {
-//         for(int t=0; t<time; t++)
-//         {
-//             YR << yr[k][t] << " ";
-//         }
-//         YR << "\n";
-//     }
-//     YR.close();
-
-//     std::ofstream YI{"data/YI.dat"};
-//     for(int k=0; k<sites; k++)
-//     {
-//         for(int t=0; t<time; t++)
-//         {
-//             YI << yi[k][t] << " ";
-//         }
-//         YI << "\n";
-//     }
-//     YI.close();
-//     return 0;
-// };
+}
--- project_2_ed/code/python/plot.py
+++ project_2_ed/code/python/plot.py
@@ -7,23 +7,53 @@ from universal_code.python.saveFig import saveFig
 
 CMAP = 'plasma'
 MAXT = 100
-N = 100
+N = 50
 
 def plot():
     data_path = f"data/"
-    yreal = np.loadtxt(data_path+f"YR.dat")
-    yimaginary = np.loadtxt(data_path+f"YI.dat")
-    
-    probability = np.abs(yreal)**2+np.abs(yimaginary)**2
-    X, T = np.meshgrid(np.linspace(-1,1,N), range(MAXT), indexing='ij')
-    
+    probability = np.loadtxt(data_path+f'P.dat')
+    mean_x = np.loadtxt(data_path+f'mean_x.dat')
+    var = np.loadtxt(data_path+'variance.dat')
+    initial_condition = np.loadtxt(data_path+'init.dat')
+    potential = np.loadtxt(data_path+'V.dat')
+
+    x = np.linspace(-1,1,N)
+    X, T = np.meshgrid(x, range(MAXT), indexing='ij')
     fig1, ax1 = plt.subplots(figsize=(12,8))
-    plot1 = ax1.pcolormesh(T, X, probability, cmap=CMAP, label='Numerical Solution')
+    plot1 = ax1.pcolormesh(T, X, probability, cmap=CMAP, shading='auto', label='Numerical Solution')
     ax1.set_xlabel("t")
-    ax1.set_ylabel("n")
+    ax1.set_ylabel("site")
     fig1.colorbar(plot1)
     ax1.set_title("Time Evolution of Probability")
     saveFig(fig_id="time_evol_plot", destination="data/")
+
+    _, ax2 = plt.subplots()
+    ax2.plot(x, potential)
+    ax2.set_title('Potential')
+    ax2.set_xlabel('x')
+    ax2.set_ylabel('V')
+    saveFig(fig_id='Potenial.png', destination="data/")
+
+    _, ax3 = plt.subplots()
+    ax3.plot(x,initial_condition)
+    ax3.set_title('Initial Wavefunction')
+    ax3.set_xlabel('x')
+    ax3.set_ylabel('$\psi_{0}(x)$')
+    saveFig(fig_id='Initial_wavefunction.png', destination="data/")
+
+    _, ax4 = plt.subplots()
+    ax4.plot(range(100), mean_x)
+    ax4.set_ylabel(r'$\langle x \rangle$')
+    ax4.set_xlabel('t')
+    ax4.set_title('Mean value of position')
+    saveFig(fig_id='mean_value_x.png', destination="data/")
+
+    _, ax5 = plt.subplots()
+    ax5.plot(range(100), var)
+    ax5.set_xlabel('t')
+    ax5.set_ylabel('$\Delta$x')
+    ax5.set_title('Uncertainty of position')
+    saveFig(fig_id='uncertainty_of_x.png', destination="data/")
     
 def main():
     plot()
